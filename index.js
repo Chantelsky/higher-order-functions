23cec7c1b721e04be26d5d3abdf5ead0be775d97
Chantel Sky <laver.chantelsky@gmail.com>
2021-03-13 09:02:15 +1000

    challenge 2

--- index.js
+++ index.js
@@ -8,18 +8,20 @@ console.log('Hello, world!');
 
 
 // Challenge 1
+//Create a function addTwo that accepts one input and adds 2 to it.
 const addTwo = (num) => {
   return num + 2
 };
 
 // To check if you've completed this function, uncomment these console.logs!
-console.log(addTwo(3));
-console.log(addTwo(10));
+// console.log(addTwo(3));
+// console.log(addTwo(10));
 
 
 // Challenge 2
+//Create a function addS that accepts one input and adds an "s" to it.
 const addS = (word) => {
-
+  return word + "s"
 };
 
 // Uncomment these to check your work
