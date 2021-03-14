b197cd0bd1519c475a2632038ea7681f3584a635
Chantel Sky <laver.chantelsky@gmail.com>
2021-03-14 12:50:45 +1000

    challenge 7 - using reduce and filter

--- index.js
+++ index.js
@@ -80,11 +80,13 @@ console.log(reduce(nums, add, 0));
 
 // Challenge 7
 //Construct a function intersection that compares input arrays and returns a new array with elements found in all of the inputs. BONUS: Use reduce!
-const intersection = (arrays) => {
-
+const intersection = (...arrays) => {
+  return arrays.reduce((acc, arrays) => {
+    return arrays.filter((item) => acc.includes(item));
+  })
 };
 
-// console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]));
+console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]));
 // should log: [5, 15]
 
 
